b2b51f06cbf73eba42b831361b31f3e74a9db2d8
nikita-plotnytskyi <nikita.plotnitckiy@gmail.com>
2025-12-26 03:09:58 +0200

    fix resize logic

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from typing import Any
 class Dictionary:
 
     def __init__(self) -> None:
+        self.index_key = None
         self.my_dict = ["", "", "", "", "", "", "", "", ]
         self.max_length = 8
         self.max_items = self.max_length * 2 // 3
@@ -11,27 +12,25 @@ class Dictionary:
 
     def __setitem__(self, key: Any, value: Any) -> None:
         self.hash_key = hash(key)
-        if self.count_items >= self.max_items:
-            self.old_list = self.reset_dict()
-            for item in self.old_list:
-                if item != "":
-                    self.add_new_item(*item)
+        self.index_key = self.hash_key % self.max_length
 
-        self.add_new_item(self.hash_key, key, value)
+        if self.check_item(self.hash_key, key):
+            self.update(self.hash_key, key, value)
+        else:
+            if self.count_items >= self.max_items:
+                self.old_list = self.reset_dict()
+                self.old_list.append((self.hash_key, key, value))
+                for item in self.old_list:
+                    if item != "":
+                        self.add_new_item(*item)
+            else:
+                self.add_new_item(self.hash_key, key, value)
 
     def __getitem__(self, key: Any) -> Any:
-        index_key = hash(key) % self.max_length
-        change_index = index_key
         try:
-            while True:
-                if (
-                    self.my_dict[change_index][1] == key
-                    and self.my_dict[change_index][0] == hash(key)
-                ):
-                    return self.my_dict[change_index][2]
-                change_index = (change_index + 1) % self.max_length
-                if change_index == index_key:
-                    raise KeyError(key)
+            self.index_key = hash(key) % self.max_length
+            self.check_item(hash(key), key)
+            return self.my_dict[self.index_key][2]
         except IndexError:
             raise KeyError(key)
 
@@ -44,24 +43,31 @@ class Dictionary:
             key: Any,
             value: Any
     ) -> None:
-        index_key = hash_key % self.max_length
-
-        while self.my_dict[index_key] != "":
-            if (
-                self.my_dict[index_key][1] == key
-                and self.my_dict[index_key][0] == hash_key
-            ):
-                self.my_dict[index_key] = (hash_key, key, value)
-                return
-            index_key = (index_key + 1) % self.max_length
-
+        self.index_key = hash_key % self.max_length
+        self.check_item(hash_key, key)
         self.count_items += 1
-        self.my_dict[index_key] = (hash_key, key, value)
+        self.my_dict[self.index_key] = (hash_key, key, value)
 
     def reset_dict(self) -> list:
+        self.index_key = None
         self.max_length *= 2
         self.max_items = self.max_length * 2 // 3
         self.count_items = 0
         old_dict = self.my_dict.copy()
         self.my_dict = [""] * self.max_length
         return old_dict
+
+    def check_item(self, hash_key: int, key: Any) -> bool:
+        self.check_index = self.index_key
+        while True:
+            if self.my_dict[self.index_key] == "":
+                return False
+            elif (self.my_dict[self.index_key][1] == key
+                  and self.my_dict[self.index_key][0] == hash_key):
+                return True
+            self.index_key = (self.index_key + 1) % self.max_length
+            if self.check_index == self.index_key:
+                return False
+
+    def update(self, hash_key: int, key: Any, value: Any) -> None:
+        self.my_dict[self.index_key] = (hash_key, key, value)
